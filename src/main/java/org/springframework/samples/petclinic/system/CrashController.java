97c88a2c1af40b181eab94e8d6acd243907fb3bd
Varun Kaushik <87529836+Varun9650@users.noreply.github.com>
2025-05-22 15:17:58 +0530

    Update CrashController.java

--- src/main/java/org/springframework/samples/petclinic/system/CrashController.java
+++ src/main/java/org/springframework/samples/petclinic/system/CrashController.java
@@ -27,11 +27,11 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 class CrashController {
-
-    //@GetMapping("/oups")
-    // public String triggerException() {
-    //     throw new RuntimeException(
-    //             "Expected: controller used to showcase what happens when an exception is thrown");
-    // }
+	private boolean crashEnabled;
+    @GetMapping("/oups")
+    public String triggerException() {
+        throw new RuntimeException(
+                "Expected: controller used to showcase what happens when an exception is thrown");
+    }
 
 }
